b7d2f222ca230ab7a7373424eeaa4e44ccd8fd4b
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-07-04 18:53:27 +0300

    commit

--- src/get-foto.js
+++ src/get-foto.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const PIXABAY_KEY = "28403201-b05b1c74044ac0f199d732ec5"
 
-export const fetchPhotos = async (searchValue) => {
+export const fetchPhotos = async (searchValue, page, per_page) => {
 
 const options = new URLSearchParams({
     key: PIXABAY_KEY,
@@ -10,10 +10,12 @@ const options = new URLSearchParams({
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
-    per_page: 40,
+    per_page,
+    page,
 })
 
 axios.defaults.baseURL = "https://pixabay.com/api/"
 
-return await axios.get(`?${options}`)
+    const { data } = await axios.get(`?${options}`)
+    return data
 }
--- src/index.js
+++ src/index.js
@@ -3,28 +3,38 @@ import { Notify } from 'notiflix';
 
 const search = document.querySelector(".search-form")
 const gallery = document.querySelector(".gallery")
+const loadMoreButton = document.querySelector(".load-more")
 let markup = ""
+let page
+let per_page = 40
+let inputValue = ""
+
 search.addEventListener("submit", onSubmitForm)
+loadMoreButton.addEventListener("click", onLoadMore)
 
 function onSubmitForm(event) {
   event.preventDefault();
-  const inputValue = event.target.elements.searchQuery.value
+  page = 1
+  inputValue = event.target.elements.searchQuery.value
   
-  fetchPhotos(inputValue)
+  fetchPhotos(inputValue, page, per_page)
     .then((images) => {
-      console.log(images.data.hits.length)
-      if(images.data.hits.length === 0){
-        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-        gallery.innerHTML = ""
-        return
-      }
-        gallery.innerHTML = ""
-        renderImages(images.data.hits)
+      console.log(images.hits.length)
+      console.log(images)
+      // if(images.hits.length === 0){
+      //   Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+      //   gallery.innerHTML = ""
+      //   return
+      // }
+      //   gallery.innerHTML = ""
+      //   renderImages(images.hits)
       })
-      .catch(error => console.log(error))
+    .catch(error => console.log(error))
+  
 }
 
 function renderImages(images) {
+  
   images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => { 
   markup = `<div class="photo-card">
             <img src="${webformatURL}" alt="${tags}" loading="lazy" />
@@ -45,4 +55,23 @@ function renderImages(images) {
             </div>`
   gallery.insertAdjacentHTML('afterbegin', markup)
   }).join("")
+}
+
+function onLoadMore() {
+  page +=1
+  console.log(page)  
+
+    fetchPhotos(inputValue, page, per_page)
+    .then((images) => {
+      console.log(images.hits.length)
+      console.log(images)
+      // if(images.hits.length === 0){
+      //   Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+      //   gallery.innerHTML = ""
+      //   return
+      // }
+      //   gallery.innerHTML = ""
+      //   renderImages(images.hits)
+      })
+    .catch(error => console.log(error))
 }
