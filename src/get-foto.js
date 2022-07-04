1ee95d5f2b5c3ffa82d5178c9d823702fd90d6e9
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-07-04 15:58:41 +0300

    add render images

--- src/get-foto.js
+++ src/get-foto.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const PIXABAY_KEY = "28403201-b05b1c74044ac0f199d732ec5"
 
-export const fetchPhotos = (searchValue) => {
+export const fetchPhotos = async (searchValue) => {
 
 const options = new URLSearchParams({
     key: PIXABAY_KEY,
@@ -10,9 +10,10 @@ const options = new URLSearchParams({
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
+    per_page: 40,
 })
 
 axios.defaults.baseURL = "https://pixabay.com/api/"
 
-return axios.get(`?${options}`)
+return await axios.get(`?${options}`)
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
 import { fetchPhotos } from "./get-foto";
+import { Notify } from 'notiflix';
 
 const search = document.querySelector(".search-form")
-
+const gallery = document.querySelector(".gallery")
+let markup = ""
 search.addEventListener("submit", onSubmitForm)
 
 function onSubmitForm(event) {
-     event.preventDefault();
-     const inputValue = event.target.elements.searchQuery.value
-     console.log(inputValue)
-     console.log(fetchPhotos(inputValue))
+  event.preventDefault();
+  const inputValue = event.target.elements.searchQuery.value
+  
+  fetchPhotos(inputValue)
+    .then((images) => {
+      console.log(images.data.hits.length)
+      if(images.data.hits.length === 0){
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        gallery.innerHTML = ""
+        return
+      }
+        gallery.innerHTML = ""
+        renderImages(images.data.hits)
+      })
+      .catch(error => console.log(error))
 }
 
+function renderImages(images) {
+  images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => { 
+  markup = `<div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+            <p class="info-item">
+            <b>Likes: ${likes}</b>
+            </p>
+            <p class="info-item">
+            <b>Views: ${views}</b>
+            </p>
+            <p class="info-item">
+            <b>Comments: ${comments}</b>
+            </p>
+            <p class="info-item">
+            <b>Downloads: ${downloads}</b>
+            </p>
+            </div>
+            </div>`
+  gallery.insertAdjacentHTML('afterbegin', markup)
+  }).join("")
+}
