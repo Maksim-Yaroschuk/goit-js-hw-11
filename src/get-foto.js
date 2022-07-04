e3ff7963fa2f94c54e6fbd374606a5cf546f6084
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-07-04 22:34:44 +0300

    add first working version

--- src/get-foto.js
+++ src/get-foto.js
@@ -2,20 +2,50 @@ import axios from "axios";
 
 const PIXABAY_KEY = "28403201-b05b1c74044ac0f199d732ec5"
 
-export const fetchPhotos = async (searchValue, page, per_page) => {
-
-const options = new URLSearchParams({
-    key: PIXABAY_KEY,
-    q: searchValue,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: true,
-    per_page,
-    page,
-})
+// export const fetchPhotos = async (searchValue, page, per_page) => {
 
 axios.defaults.baseURL = "https://pixabay.com/api/"
 
-    const { data } = await axios.get(`?${options}`)
-    return data
+//     const { data } = await axios.get(`?${options}`)
+//     return data
+// }
+
+export class GetFotoPixabay {
+    constructor() {
+        this.searchValue = ""
+        this.page = 1
+    }
+
+    async fetchPhotos() {
+       
+        const options = new URLSearchParams({
+            key: PIXABAY_KEY,
+            q: this.searchValue,
+            image_type: "photo",
+            orientation: "horizontal",
+            safesearch: true,
+            per_page: 6,
+            page: this.page,
+})
+
+        const { data } = await axios.get(`?${options}`)
+        this.incrPage()
+        return data 
+    }
+
+    get searchValue1() {
+        return this.searchValue
+    }
+
+    set searchValue1(newSearchValue) {
+        this.searchValue = newSearchValue
+    }
+
+    incrPage() {
+       this.page +=1
+    }
+
+    resetPage() {
+        this.page = 1
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,43 +1,23 @@
-import { fetchPhotos } from "./get-foto";
+// import { fetchPhotos } from "./get-foto";
 import { Notify } from 'notiflix';
+import { GetFotoPixabay } from "./get-foto"
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css"
 
-const search = document.querySelector(".search-form")
+// let inputValue = ""
+
 const gallery = document.querySelector(".gallery")
+const search = document.querySelector(".search-form")
 const loadMoreButton = document.querySelector(".load-more")
-let markup = ""
-let page
-let per_page = 40
-let inputValue = ""
 
 search.addEventListener("submit", onSubmitForm)
-loadMoreButton.addEventListener("click", onLoadMore)
+loadMoreButton.addEventListener("click", onLoadMorePhotos)
 
-function onSubmitForm(event) {
-  event.preventDefault();
-  page = 1
-  inputValue = event.target.elements.searchQuery.value
-  
-  fetchPhotos(inputValue, page, per_page)
-    .then((images) => {
-      console.log(images.hits.length)
-      console.log(images)
-      // if(images.hits.length === 0){
-      //   Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-      //   gallery.innerHTML = ""
-      //   return
-      // }
-      //   gallery.innerHTML = ""
-      //   renderImages(images.hits)
-      })
-    .catch(error => console.log(error))
-  
-}
+const getFotoPixabay = new GetFotoPixabay()
 
-function renderImages(images) {
-  
-  images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => { 
-  markup = `<div class="photo-card">
-            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+function createdMarkup(images) {
+  return images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
+           <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" />
             <div class="info">
             <p class="info-item">
             <b>Likes: ${likes}</b>
@@ -51,27 +31,141 @@ function renderImages(images) {
             <p class="info-item">
             <b>Downloads: ${downloads}</b>
             </p>
+            </a>
             </div>
-            </div>`
-  gallery.insertAdjacentHTML('afterbegin', markup)
-  }).join("")
+            </div>`).join("")
 }
 
-function onLoadMore() {
-  page +=1
-  console.log(page)  
-
-    fetchPhotos(inputValue, page, per_page)
-    .then((images) => {
-      console.log(images.hits.length)
-      console.log(images)
-      // if(images.hits.length === 0){
-      //   Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-      //   gallery.innerHTML = ""
-      //   return
-      // }
-      //   gallery.innerHTML = ""
-      //   renderImages(images.hits)
-      })
-    .catch(error => console.log(error))
+function renderMarkup(images) {
+  gallery.insertAdjacentHTML("beforeend", createdMarkup(images))
 }
+
+async function onSubmitForm(event) {
+  event.preventDefault()
+  clear()
+  const inputValue = event.target.elements.searchQuery.value
+console.log(inputValue)
+
+  getFotoPixabay.searchValue1 = inputValue
+  
+  try {
+    const { hits, totalHits } = await getFotoPixabay.fetchPhotos()
+  if (totalHits === 0) {
+    Notify.warning("Sorry, there are no images matching your search query. Please try again.")
+    event.target.reset()
+    return
+  }
+    Notify.success(`Hooray! We found ${totalHits} images.`)
+    renderMarkup(hits)
+    lightbox.refresh()
+  } catch (error) { Notify.failure(error.message) }
+  // console.log(hits)
+  // console.log(totalHits)
+
+
+  // event.target.elements.searchQuery.value = ""
+  event.target.reset()
+// return inputValue = event.target.elements.searchQuery.value
+}
+
+function clear() {
+  gallery.innerHTML = ""
+}
+
+async function onLoadMorePhotos() {
+  try {
+  const { hits, totalHits } = await getFotoPixabay.fetchPhotos()
+    renderMarkup(hits)
+    lightbox.refresh()
+
+    const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+
+    } catch (error) { Notify.failure(error.message) }
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+  captionsData: "alt",
+  captionDelay: 500,
+ });
+
+// const search = document.querySelector(".search-form")
+
+// const loadMoreButton = document.querySelector(".load-more")
+// let markup = ""
+// let page
+// let per_page = 40
+// let inputValue = ""
+
+// search.addEventListener("submit", onSubmitForm)
+// loadMoreButton.addEventListener("click", onLoadMore)
+
+// function onSubmitForm(event) {
+//   event.preventDefault();
+//   page = 1
+//   inputValue = event.target.elements.searchQuery.value
+  
+//   fetchPhotos(inputValue, page, per_page)
+//     .then((images) => {
+//       console.log(images.hits.length)
+//       console.log(images)
+//       // if(images.hits.length === 0){
+//       //   Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+//       //   gallery.innerHTML = ""
+//       //   return
+//       // }
+//       //   gallery.innerHTML = ""
+//       //   renderImages(images.hits)
+//       })
+//     .catch(error => console.log(error))
+  
+// }
+
+// function renderImages(images) {
+  
+//   images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => { 
+//   markup = `<div class="photo-card">
+//             <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//             <div class="info">
+//             <p class="info-item">
+//             <b>Likes: ${likes}</b>
+//             </p>
+//             <p class="info-item">
+//             <b>Views: ${views}</b>
+//             </p>
+//             <p class="info-item">
+//             <b>Comments: ${comments}</b>
+//             </p>
+//             <p class="info-item">
+//             <b>Downloads: ${downloads}</b>
+//             </p>
+//             </div>
+//             </div>`
+//   gallery.insertAdjacentHTML('afterbegin', markup)
+//   }).join("")
+// }
+
+// function onLoadMore() {
+//   page +=1
+//   console.log(page)  
+
+//     fetchPhotos(inputValue, page, per_page)
+//     .then((images) => {
+//       console.log(images.hits.length)
+//       console.log(images)
+//       // if(images.hits.length === 0){
+//       //   Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+//       //   gallery.innerHTML = ""
+//       //   return
+//       // }
+//       //   gallery.innerHTML = ""
+//       //   renderImages(images.hits)
+//       })
+//     .catch(error => console.log(error))
+// }
